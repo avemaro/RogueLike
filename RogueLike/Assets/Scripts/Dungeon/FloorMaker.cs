8fbbff22e3ca164d16242ae7ca8e6621522a537f
avemaro <yamasakikota@gmail.com>
2020-04-08 23:30:42 +0900

    FloorWithOneRoom1

--- RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
+++ RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FloorMaker {
+    static int width = 30;
+    static int height = 30;
+
+public static Floor Create(int roomCount) {
+        var data = new List<string>();
+        for (var y = 0; y < height; y++) {
+            var line = "";
+            for (var x = 0; x < width; x++) {
+                if (x == 2 && y == 1) line += "試";
+                else if (x == 3 && y == 2) line += "階";
+                else line += "◆";
+            }
+            data.Add(line);
+        }
+
+        return new Floor(data.ToArray());;
+    }
+
     //public static Floor Create(string[] data) {
     //    var floor = new Floor(data);
     //    floor.Rooms.Add(new Room((2, 1), (8, 9)));
