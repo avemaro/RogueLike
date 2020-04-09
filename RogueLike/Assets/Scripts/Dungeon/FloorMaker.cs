41a18cbb6774e8a0a47097023b5e0f6cd9d200b2
avemaro <yamasakikota@gmail.com>
2020-04-10 00:27:38 +0900

    FloorWithThreeRoom_REFACTORING4

--- RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
+++ RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
@@ -15,23 +15,26 @@ public static Floor Create(int roomCount) {
             return floor;
         }
 
+        var boardX = 0;
         var boardY = 0;
-        if (roomCount >= 2) {
+        var iter = 2;
+        if (roomCount >= iter) {
             boardY = Random.Range(height * 1 / 4, height * 3 / 4);
             var room0 = new Room(floor, (0, 0), (width - 1, boardY - 1), Direction.down);
             floor.Rooms.Add(room0);
 
-            if (roomCount < 3) {
+            if (roomCount < iter + 1) {
                 var room1 = new Room(floor, (0, boardY), (width - 1, height - 1), Direction.up);
                 floor.Rooms.Add(room1);
             }
         }
-        if (roomCount >= 3) {
-            var boardX = Random.Range(width * 1 / 4, width * 3 / 4);
+        if (roomCount >= iter + 1) {
+            boardX = Random.Range(width * 1 / 4, width * 3 / 4);
             var room1 = new Room(floor, (0, boardY), (boardX - 1, height - 1), Direction.up, Direction.right);
+            floor.Rooms.Add(room1);
+
             var room2 = new Room(floor, (boardX, boardY), (width - 1, height - 1), Direction.up, Direction.left);
 
-            floor.Rooms.Add(room1);
             floor.Rooms.Add(room2);
 
             for (var y = boardY; y < height; y++)
@@ -48,6 +51,24 @@ public static Floor Create(int roomCount) {
                 floor.SetTerrain(boardX, y, TerrainType.wall);
             }
         }
+
+        for (var x = 0; x < width; x++)
+            floor.SetTerrain(x, boardY, TerrainType.land);
+
+        for (var x = 0; x < width; x++) {
+            var isUpperAisle = floor.GetTerrain(x, boardY - 1) == TerrainType.land;
+            var isLowerAisle = floor.GetTerrain(x, boardY + 1) == TerrainType.land;
+            if (isUpperAisle || isLowerAisle) break;
+            floor.SetTerrain(x, boardY, TerrainType.wall);
+        }
+        for (var x = width - 1; x > 0; x--) {
+            var isUpperAisle = floor.GetTerrain(x, boardY - 1) == TerrainType.land;
+            var isLowerAisle = floor.GetTerrain(x, boardY + 1) == TerrainType.land;
+            if (isUpperAisle || isLowerAisle) break;
+            floor.SetTerrain(x, boardY, TerrainType.wall);
+        }
+
+        return floor;
 
 
         //} else {
@@ -77,24 +98,5 @@ public static Floor Create(int roomCount) {
 
         //}
         //}
-
-        for (var x = 0; x < width; x++)
-            floor.SetTerrain(x, boardY, TerrainType.land);
-
-        for (var x = 0; x < width; x++) {
-            var isUpperAisle = floor.GetTerrain(x, boardY - 1) == TerrainType.land;
-            var isLowerAisle = floor.GetTerrain(x, boardY + 1) == TerrainType.land;
-            if (isUpperAisle || isLowerAisle) break;
-            floor.SetTerrain(x, boardY, TerrainType.wall);
-        }
-        for (var x = width - 1; x > 0; x--) {
-            var isUpperAisle = floor.GetTerrain(x, boardY - 1) == TerrainType.land;
-            var isLowerAisle = floor.GetTerrain(x, boardY + 1) == TerrainType.land;
-            if (isUpperAisle || isLowerAisle) break;
-            floor.SetTerrain(x, boardY, TerrainType.wall);
-        }
-
-
-        return floor;
     }
 }
