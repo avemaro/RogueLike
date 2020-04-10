c7b9a7d8ff4dfeff4d08e298edf06d1768fe0837
avemaro <yamasakikota@gmail.com>
2020-04-10 23:39:53 +0900

    FloorWithRooms_REFACTORING2

--- RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
+++ RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
@@ -8,27 +8,25 @@ public class FloorMaker {
 
 public static Floor Create(int roomCount) {
         var floor = new Floor(width, height);
-
         new Room(floor, (0, 0), (width - 1, height - 1));
 
-
-        for (var x = 0; x < width; x++) {
-            if (floor.GetTerrain(x, 0) == TerrainType.land)
+        for (var x = 0; x < width; x++) 
+            if (floor.GetTerrain(x, 0) == TerrainType.land ||
+                floor.GetTerrain(x, height - 1) == TerrainType.land) {
                 for (var y = 0; y < height; y++)
                     if (SetWall(x, 1, y, 0)) break;
-            if (floor.GetTerrain(x, height-1) == TerrainType.land)
                 for (var y = 0; y < height; y++)
                     if (SetWall(x, 1, height - y - 1, 0)) break;
-        }
+            }
 
-        for (var y = 0; y < height; y++) {
-            if (floor.GetTerrain(0, y) == TerrainType.land)
+        for (var y = 0; y < height; y++) 
+            if (floor.GetTerrain(0, y) == TerrainType.land ||
+                floor.GetTerrain(width - 1, y) == TerrainType.land) {
                 for (var x = 0; x < width; x++)
                     if (SetWall(x, 0, y, 1)) break;
-            if (floor.GetTerrain(width - 1, y) == TerrainType.land)
                 for (var x = 0; x < width; x++)
                     if (SetWall(width - x - 1, 0, y, 1)) break;
-        }
+            }
 
         bool SetWall(int x, int dx, int y, int dy) {
             if (floor.GetTerrain(x - dx, y - dy) == TerrainType.land ||
