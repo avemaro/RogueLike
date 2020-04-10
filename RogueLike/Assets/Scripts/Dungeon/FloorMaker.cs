4c81dc783c9297074237687f947f04d72ec0a303
avemaro <yamasakikota@gmail.com>
2020-04-10 22:11:54 +0900

    FloorWithRooms_GREEN

--- RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
+++ RogueLike/Assets/Scripts/Dungeon/FloorMaker.cs
@@ -11,70 +11,44 @@ public static Floor Create(int roomCount) {
 
         new Room(floor, (0, 0), (width - 1, height - 1));
 
+        for (var x = 0; x < width; x++) {
+            if (floor.GetTerrain(x, 0) == TerrainType.land) {
+                for (var y = 0; y < height; y++) {
+                    if (floor.GetTerrain(x - 1, y) == TerrainType.land ||
+                        floor.GetTerrain(x + 1, y) == TerrainType.land)
+                        break;
+                    floor.SetTerrain(x, y, TerrainType.wall);
+                }
+            }
+            if (floor.GetTerrain(x, height-1) == TerrainType.land) {
+                for (var y = height - 1; y > 0; y--) {
+                    if (floor.GetTerrain(x - 1, y) == TerrainType.land ||
+                        floor.GetTerrain(x + 1, y) == TerrainType.land)
+                        break;
+                    floor.SetTerrain(x, y, TerrainType.wall);
+                }
+            }
+        }
+
+        for (var y = 0; y < height; y++) {
+            if (floor.GetTerrain(0, y) == TerrainType.land) {
+                for (var x = 0; x < width; x++) {
+                    if (floor.GetTerrain(x, y - 1) == TerrainType.land ||
+                        floor.GetTerrain(x, y + 1) == TerrainType.land)
+                        break;
+                    floor.SetTerrain(x, y, TerrainType.wall);
+                }
+            }
+            if (floor.GetTerrain(width - 1, y) == TerrainType.land) {
+                for (var x = width - 1; x > 0; x--) {
+                    if (floor.GetTerrain(x, y - 1) == TerrainType.land ||
+                        floor.GetTerrain(x, y + 1) == TerrainType.land)
+                        break;
+                    floor.SetTerrain(x, y, TerrainType.wall);
+                }
+            }
+        }
+
         return floor;
-
-        //if (roomCount == 1) {
-        //    var room = new Room(floor, (0, 0), (width - 1, height - 1));
-        //    floor.Rooms.Add(room);
-        //    return floor;
-        //}
-
-        //var boardX = 0;
-        //var boardY = 0;
-        //var iter = 2;
-        //if (roomCount >= iter) {
-        //    boardY = Random.Range(height * 1 / 4, height * 3 / 4);
-        //    var room0 = new Room(floor, (0, 0), (width - 1, boardY - 1), Direction.down);
-        //    floor.Rooms.Add(room0);
-
-        //    if (roomCount < iter + 1) {
-        //        var room1 = new Room(floor, (0, boardY), (width - 1, height - 1), Direction.up);
-        //        floor.Rooms.Add(room1);
-        //    }
-        //}
-        //if (roomCount >= iter + 1) {
-        //    boardX = Random.Range(width * 1 / 4, width * 3 / 4);
-        //    var room1 = new Room(floor, (0, boardY), (boardX - 1, height - 1), Direction.up, Direction.right);
-        //    floor.Rooms.Add(room1);
-
-        //    if (roomCount < iter + 2) {
-        //        var room2 = new Room(floor, (boardX, boardY), (width - 1, height - 1), Direction.up, Direction.left);
-        //        floor.Rooms.Add(room2);
-        //    }
-
-        //    for (var y = boardY; y < height; y++)
-        //        floor.SetTerrain(boardX, y, TerrainType.land);
-
-        //    for (var y = boardY; y < height; y++) {
-        //        if (floor.GetTerrain(boardX - 1, y) == TerrainType.land) break;
-        //        if (floor.GetTerrain(boardX + 1, y) == TerrainType.land) break;
-        //        floor.SetTerrain(boardX, y, TerrainType.wall);
-        //    }
-        //    for (var y = height - 1; y > boardY; y--) {
-        //        if (floor.GetTerrain(boardX - 1, y) == TerrainType.land) break;
-        //        if (floor.GetTerrain(boardX + 1, y) == TerrainType.land) break;
-        //        floor.SetTerrain(boardX, y, TerrainType.wall);
-        //    }
-        //}
-
-        //for (var x = 0; x < width; x++)
-        //    floor.SetTerrain(x, boardY, TerrainType.land);
-
-        //for (var x = 0; x < width; x++) {
-        //    var isUpperAisle = floor.GetTerrain(x, boardY - 1) == TerrainType.land;
-        //    var isLowerAisle = floor.GetTerrain(x, boardY + 1) == TerrainType.land;
-        //    if (isUpperAisle || isLowerAisle) break;
-        //    floor.SetTerrain(x, boardY, TerrainType.wall);
-        //}
-        //for (var x = width - 1; x > 0; x--) {
-        //    var isUpperAisle = floor.GetTerrain(x, boardY - 1) == TerrainType.land;
-        //    var isLowerAisle = floor.GetTerrain(x, boardY + 1) == TerrainType.land;
-        //    if (isUpperAisle || isLowerAisle) break;
-        //    floor.SetTerrain(x, boardY, TerrainType.wall);
-        //}
-
-        //return floor;
-
-
     }
 }
--- RogueLike/Assets/Scripts/Dungeon/Room.cs
+++ RogueLike/Assets/Scripts/Dungeon/Room.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Room {
     static readonly int WidthLimit = 30;
     static readonly int HeightLimit = 30;
-    static readonly int wallMin = 2;
-    static readonly int wallMax = 4;
+    static readonly int wallMin = 4;
+    static readonly int wallMax = 5;
 
     readonly Floor floor;
     readonly Cell startPoint;
@@ -14,9 +14,9 @@ public class Room {
     int Width { get => endPoint.x - startPoint.x +  1; }
     int Height { get => endPoint.y - startPoint.y + 1; }
     readonly int wallThickness;
-    readonly Direction[] neighbor;
+    readonly Direction[] neighbors;
 
-    public Room(Floor floor, (int x, int y) start, (int x, int y) end, params Direction[] neighbor) {
+    public Room(Floor floor, (int x, int y) start, (int x, int y) end, params Direction[] neighbors) {
         this.floor = floor;
         startPoint = new Cell(start.x, start.y);
         endPoint = new Cell(end.x, end.y);
@@ -28,49 +28,48 @@ public class Room {
         var divRatio = Random.Range(2f, 4f);
         var division = divisions[Random.Range(0, divisions.Count)];
         switch (division) {
-            case Division.none:
-                Debug.Log("none");
-                break;
             case Division.vertical:
-                Debug.Log("vertical");
                 var startV0 = startPoint.Tuple;
                 var endV0 = (startPoint.x + Mathf.RoundToInt(Width / divRatio) - 1, endPoint.y);
-                var directions = new List<Direction>(neighbor) {
+                var directions = new List<Direction>(neighbors) {
                     Direction.right
                 };
                 new Room(floor, startV0, endV0, directions.ToArray());
-                //new Room(floor, startV0, endV0, Direction.right);
 
                 var startV1 = (startPoint.x + Mathf.RoundToInt(Width / divRatio), startPoint.y);
                 var endV1 = endPoint.Tuple;
-                directions = new List<Direction>(neighbor) {
+                directions = new List<Direction>(neighbors) {
                     Direction.left
                 };
                 new Room(floor, startV1, endV1, directions.ToArray());
-                //new Room(floor, startV1, endV1,  Direction.left);
+
+                for (var y = startPoint.y; y < endPoint.y + 1; y++)
+                    floor.SetTerrain(endV0.Item1, y, TerrainType.land);
+
                 return;
             case Division.horizonal:
-                Debug.Log("horizonal");
                 var startH0 = startPoint.Tuple;
                 var endH0 = (endPoint.x, startPoint.y + Height / 2 - 1);
-                directions = new List<Direction>(neighbor) {
+                directions = new List<Direction>(neighbors) {
                     Direction.down
                 };
                 new Room(floor, startH0, endH0, directions.ToArray());
-                //new Room(floor, startH0, endH0, Direction.down);
 
                 var startH1 = (startPoint.x, startPoint.y + Height / 2);
                 var endH1 = endPoint.Tuple;
-                directions = new List<Direction>(neighbor) {
+                directions = new List<Direction>(neighbors) {
                     Direction.up
                 };
                 new Room(floor, startH1, endH1, directions.ToArray());
-                //new Room(floor, startH1, endH1, Direction.up);
+
+                for (var x = startPoint.x; x < endPoint.x + 1; x++)
+                    floor.SetTerrain(x, endH0.Item2, TerrainType.land);
+
                 return;
 
         }
         wallThickness = Random.Range(wallMin, wallMax);
-        this.neighbor = neighbor;
+        this.neighbors = neighbors;
         CreateLand();
         CreateExit();
         floor.Rooms.Add(this);
@@ -95,60 +94,35 @@ public class Room {
     }
 
     void CreateExit() {
-        if (neighbor.Length == 0) return;
+        foreach (var neighbor in neighbors) {
+            int x = 0;
+            int y = 0;
+            if (neighbor == Direction.up || neighbor == Direction.down) {
+                x = startPoint.x + Random.Range(wallThickness, Width - wallThickness);
+                if (neighbor == Direction.up)
+                    y = startPoint.y;
+                if (neighbor == Direction.down)
+                    y = endPoint.y;
+                var position = new Cell(x, y);
+                for (var i = 0; i < wallThickness; i++) {
+                    floor.SetTerrain(position.x, position.y, TerrainType.land);
+                    position = position.Next(neighbor.Reverse());
+                }
+            }
 
-        int x = 0;
-        int y = 0;
-        if (neighbor[0] == Direction.up || neighbor[0] == Direction.down) {
-            x = startPoint.x + Random.Range(wallThickness, Width - wallThickness);
-            if (neighbor[0] == Direction.up)
-                y = startPoint.y;
-            if (neighbor[0] == Direction.down)
-                y = endPoint.y;
-            var position = new Cell(x, y);
-            for (var i = 0; i < wallThickness; i++) {
-                floor.SetTerrain(position.x, position.y, TerrainType.land);
-                position = position.Next(neighbor[0].Reverse());
+            if (neighbor == Direction.left || neighbor == Direction.right) {
+                y = startPoint.y + Random.Range(wallThickness, Height - wallThickness);
+                if (neighbor == Direction.right)
+                    x = endPoint.x;
+                if (neighbor == Direction.left)
+                    x = startPoint.x;
+                var position = new Cell(x, y);
+                for (var i = 0; i < wallThickness; i++) {
+                    floor.SetTerrain(position.x, position.y, TerrainType.land);
+                    position = position.Next(neighbor.Reverse());
+                }
             }
         }
-
-        if (neighbor[0] == Direction.left || neighbor[0] == Direction.right) {
-            y = startPoint.y + Random.Range(wallThickness, Height - wallThickness);
-            if (neighbor[0] == Direction.right)
-                x = endPoint.x;
-            if (neighbor[0] == Direction.left)
-                x = startPoint.x;
-            var position = new Cell(x, y);
-            for (var i = 0; i < wallThickness; i++) {
-                floor.SetTerrain(position.x, position.y, TerrainType.land);
-                position = position.Next(neighbor[0].Reverse());
-            }
-        }
-
-        //int x = startPoint.x + Random.Range(wallThickness, Width - wallThickness);
-        //int y = 0;
-        //if (neighbor[0] == Direction.down)
-        //    y = endPoint.y;
-        //if (neighbor[0] == Direction.up)
-        //    y = startPoint.y;
-        //var position = new Cell(x, y);
-        //for (var i = 0; i < wallThickness; i++) {
-        //    floor.SetTerrain(position.x, position.y, TerrainType.land);
-        //    position = position.Next(neighbor[0].Reverse());
-        //}
-
-        //if (neighbor.Length < 2) return;
-
-        //y = startPoint.y + Random.Range(wallThickness, Height - wallThickness);
-        //if (neighbor[1] == Direction.right)
-        //    x = endPoint.x;
-        //if (neighbor[1] == Direction.left)
-        //    x = startPoint.x;
-        //position = new Cell(x, y);
-        //for (var i = 0; i < wallThickness; i++) {
-        //    floor.SetTerrain(position.x, position.y, TerrainType.land);
-        //    position = position.Next(neighbor[1].Reverse());
-        //}
     }
 
     enum Division {
