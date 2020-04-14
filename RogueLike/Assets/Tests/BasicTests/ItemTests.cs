6ebdb6ca8ec55e7697f3c0704ae4a5a517fcbe4a
avemaro <yamasakikota@gmail.com>
2020-04-14 21:41:30 +0900

    ItemMaker_TEST

---
+++ RogueLike/Assets/Tests/BasicTests/ItemTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ItemTests
+    {
+        [Test]
+        public void CreateMedicinalHerb() {
+            var floor = FloorMaker.Create();
+            var player = floor.Player;
+
+            var item = ItemMaker.Create("MedicinalHerb");
+            var hp = player.HP;
+            player.Use(item);
+            Assert.Greater(player.HP, hp);
+        }
+    }
+}
